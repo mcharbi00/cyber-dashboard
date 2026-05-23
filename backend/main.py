b51afa8d22c0a7d04deb83fdd8ab95cd19dd2dbb
Mahmoud Charbi <mcharbi0@gmail.com>
2026-05-23 23:22:47 +0200

    ajout du scan réseau avec React et FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import socket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class ScanRequest(BaseModel):
+    ip: str
 
 @app.get("/")
 def root():
     return {"message": "Cyber Dashboard API"}
+
+@app.post("/scan")
+def scan_ports(data: ScanRequest):
+
+    ports = [21, 22, 80, 443, 3306]
+    open_ports = []
+
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+
+        result = sock.connect_ex((data.ip, port))
+
+        if result == 0:
+            open_ports.append(port)
+
+        sock.close()
+
+    return {
+        "ip": data.ip,
+        "open_ports": open_ports
+    }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,50 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
-
+  const [ip, setIp] = useState("");
+  const [ports, setPorts] = useState([]);
   useEffect(() => {
     fetch("http://127.0.0.1:8000/")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
+  const scanIP = async () => {
 
+    const response = await fetch("http://127.0.0.1:8000/scan", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ip }),
+    });
+  
+    const data = await response.json();
+  
+    setPorts(data.open_ports);
+  };
   return (
     <div>
-      <h1>Cyber Dashboard</h1>
-      <p>{message}</p>
-    </div>
+  <h1>Cyber Dashboard</h1>
+
+  <input
+    type="text"
+    placeholder="Adresse IP"
+    value={ip}
+    onChange={(e) => setIp(e.target.value)}
+  />
+
+  <button onClick={scanIP}>
+    Scanner
+  </button>
+
+  <ul>
+    {ports.map((port) => (
+      <li key={port}>
+        Port {port} ouvert
+      </li>
+    ))}
+  </ul>
+</div>
   );
 }
 
